c3cdd909ab1d36533892d6b88b9352aabaa058ce
constantinrazvan <76180811+constantinrazvan@users.noreply.github.com>
2024-07-02 14:22:05 +0300

    .

--- src/app/core/environment.ts
+++ src/app/core/environment.ts
@@ -1,7 +1,17 @@
 export const environment = {
     baseUrl: "https://localhost:5000",
-    api_login: "/api//Auth/Login",
+    api_login: "/api/Auth/Login",
     api_register: "/api/Auth/Register",
-    api_getUserData: "/api/Users/GetUser",
-    api_getAllUsers: "",
+    api_getUserData: "/api/GetUserData",
+    api_currencies_get: "/api/Currencies", 
+
+    api_economy_get: "/api/GetEconomy",
+    api_economies_get: "/api/GetEconomies",
+    api_economy_add: "/api/AddEconomy",
+    api_economy_delete: "/api/DeleteEconomy",
+
+    api_expenses_get: "/api/Expenses/GetExpenses",
+    api_expenses_getByUser: "/api/Expenses/GetExpenseByUser",
+    api_expenses_put: "/api/Expenses/AddExpense",
+    api_expenses_delete: "/api/Expenses/RemoveExpense",
 }
