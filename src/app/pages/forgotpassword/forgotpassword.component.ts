87907bd4f96fcc7e514f7f309397ac074ad8a302
georgepata <geo.patza@yahoo.com>
2024-07-02 12:20:11 +0300

    .

--- src/app/pages/forgotpassword/forgotpassword.component.ts
+++ src/app/pages/forgotpassword/forgotpassword.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { UnauthnavbarComponent } from '../../common/navbar/unauthnavbar/unauthnavbar.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-forgotpassword',
   standalone: true,
-  imports: [],
+  imports: [UnauthnavbarComponent, RouterLink, MatFormField, MatLabel, MatInput, CommonModule, ReactiveFormsModule],
   templateUrl: './forgotpassword.component.html',
   styleUrl: './forgotpassword.component.scss'
 })
 export class ForgotpasswordComponent {
-
+  form! : FormGroup;
+  emailSent! : boolean;
+  
+  constructor(private fb: FormBuilder){
+    this.emailSent = false;
+  }
+  
+  sendEmail(){
+    this.emailSent = true;
+    console.log(this.emailSent);
+  }
+  
+  ngOnInit(){
+    this.form = this.fb.group({
+      email:['', [Validators.required, Validators.email]]
+    });
+  }
 }
--- src/app/pages/resetpassword/resetpassword.component.ts
+++ src/app/pages/resetpassword/resetpassword.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { UnauthnavbarComponent } from '../../common/navbar/unauthnavbar/unauthnavbar.component';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { Form, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-resetpassword',
   standalone: true,
-  imports: [],
+  imports: [UnauthnavbarComponent, MatFormField, MatLabel, MatInput, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './resetpassword.component.html',
   styleUrl: './resetpassword.component.scss'
 })
 export class ResetpasswordComponent {
+resetPasswordForm! : FormGroup;
+
+constructor(private fb: FormBuilder){
+  this.resetPasswordForm = this.fb.group({
+    newPassword: ['', [Validators.minLength(8), Validators.required]],
+    confirmPassword: ['', [Validators.minLength(8), Validators.required]]
+  });
+}
+
+passwordMatches(resetPasswordForm: FormGroup){
+  return this.resetPasswordForm.controls["newPassword"].value === 
+      resetPasswordForm.controls["confirmPassword"].value ? null : {mismatch : true}
+}
+
+onSubmit(){
+  
+}
 
 }
