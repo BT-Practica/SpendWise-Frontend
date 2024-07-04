020f5b728199d50197fd85f452aedd3c3c7c5b71
andreialionte <andrei.alionte@gmail.com>
2024-07-04 12:07:03 +0300

    .

---
+++ src/app/core/interfaces/userGetUserById.interface.ts
@@ -0,0 +1,11 @@
+interface userGetUserById{
+    id: number;
+    username:string;
+    email:string;
+    createdDate:string
+    economies: any[];
+    incomes: any[];
+    currencyId:number;
+    expenses: any[];
+
+}
